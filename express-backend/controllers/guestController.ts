ed5a1cfc80b9fb38cd8baf83b20b1173a50e4ccd
banuka <banukaramanayaka76@gmail.com>
2024-03-28 21:34:50 +0530

    refactor:rest api guest

---
+++ express-backend/controllers/guestController.ts
@@ -0,0 +1,104 @@
+import { Request, Response, } from 'express';
+import { PrismaClient } from '@prisma/client'
+import { GuestModel } from '../prisma/zod';
+
+const prisma = new PrismaClient()
+
+//get all guests
+export const getAllGuests=async (req: Request, res: Response) => {
+    try {
+        const guest = await prisma.guest.findMany({})
+        res.json({ message: 'all guests', guest })
+    } catch (error) {
+        res.json({ error })
+    }
+
+}
+
+//get a  guest by id 
+export const getGuestById=async (req: Request, res: Response) => {
+    
+    try {
+
+        const guest = await prisma.guest.findUnique({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.json({ message: 'guest by id', guest })
+    } catch (error) {
+        res.json({ error })
+    }
+
+}
+
+//get a  guest by email
+export const getGuestByEmail=async (req: Request, res: Response) => {
+    try {
+        const guest = await prisma.guest.findUnique({
+            where: {
+                email: req.params.email
+            }
+        })
+
+        res.json({ message: 'guest by email', guest })
+    } catch (error) {
+        res.json({ error })
+    }
+}
+
+//post  creater guest
+
+export const createGuest=async (req: Request, res: Response) => {
+    const body = req.body
+    const parsedData = GuestModel.safeParse(body)
+    if (!parsedData.success) {
+        res.status(400).json({ error: 'invalid data entered' })
+        return
+    }
+    try {
+        const guest = await prisma.guest.create({
+            data: parsedData.data
+        })
+        res.status(200).json({ message: guest })
+    } catch (error) {
+        res.status(400).json({ error: error })
+    }
+
+}
+
+//delete  guest by id
+export const deleteGuest=async (req: Request, res: Response) => {
+    try {
+        const guest = await prisma.guest.delete({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.json({ message: 'guest deleted', guest })
+    } catch (error) {
+        res.json({ error })
+    }
+
+}
+
+//update guest 
+export const updateGuest=async (req: Request, res: Response) => {
+    const body = req.body
+    const parsedData = GuestModel.safeParse(body)
+    if (!parsedData.success) {
+        res.status(400).json({ error: 'invalid data entered' })
+        return
+    }
+    try {
+        const guest = await prisma.guest.update({
+            where:{id:req.params.id},
+            data: parsedData.data
+        })
+        res.status(200).json({ message: guest })
+    } catch (error) {
+        res.status(400).json({ error: error })
+    }
+
+}
+
--- express-backend/routes/guestRoutes.ts
+++ express-backend/routes/guestRoutes.ts
@@ -1,88 +1,25 @@
+import { createGuest, deleteGuest, getAllGuests, getGuestByEmail, getGuestById, updateGuest } from "../controllers/guestController"
+
 const express = require('express')
-
-import { Request, Response, } from 'express';
-import { PrismaClient } from '@prisma/client'
-import { GuestModel } from '../prisma/zod';
-
-const prisma = new PrismaClient()
-console.log('prisma client 1 created');
-
-
 
 const router = express.Router()
 
 //get all guests
-router.get('/', async (req: Request, res: Response) => {
-    try {
-        const guest = await prisma.guest.findMany({})
-        res.json({ message: 'all guests', guest })
-    } catch (error) {
-        res.json({ error })
-    }
+router.get('/',getAllGuests )
 
-})
-
-//get a  guest by id or email
-router.get('/id/:id', async (req: Request, res: Response) => {
-    // console.log(req.params);
-    try {
-
-        const guest = await prisma.guest.findUnique({
-            where: {
-                id: req.params.id
-            }
-        })
-        res.json({ message: 'guest by id', guest })
-    } catch (error) {
-        res.json({ error })
-    }
-
-})
+//get a  guest by id 
+router.get('/id/:id',getGuestById )
 
 //get a  guest by email
-router.get('/email/:email', async (req: Request, res: Response) => {
-    try {
-        const guest = await prisma.guest.findUnique({
-            where: {
-                email: req.params.email
-            }
-        })
+router.get('/email/:email',getGuestByEmail )
 
-        res.json({ message: 'guest by email', guest })
-    } catch (error) {
-        res.json({ error })
-    }
-})
-
-//post / creater guest
-router.post('/', async (req: Request, res: Response) => {
-    const body = req.body
-    const parsedData = GuestModel.safeParse(body)
-    if (!parsedData.success) {
-        res.status(400).json({ error: 'invalid data entered' })
-        return
-    }
-    try {
-        const guest = await prisma.guest.create({
-            data: parsedData.data
-        })
-        res.status(200).json({ message: guest })
-    } catch (error) {
-        res.status(400).json({ error: error })
-    }
-
-})
+//post  creater guest
+router.post('/',createGuest )
 
 //delete  guest by id
-router.delete('/:id', (req: Request, res: Response) => {
-    res.json({ message: 'delete guest' })
-
-})
+router.delete('/:id', deleteGuest)
 //update guest by id
-router.patch('/:id', (req: Request, res: Response) => {
-    res.json({ message: 'update  guest' })
-
-})
+router.patch('/:id',updateGuest)
 
 
 
